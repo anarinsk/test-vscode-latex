0f0c217d215625b08814c44607ef402057518d1c
anarinsk <anarinsk@gmail.com>
2020-10-24 13:46:12 +0900

    successfully compiled

--- install_kotex.sh
+++ install_kotex.sh
@@ -1,12 +1,9 @@
 #!/bin/sh 
 
-#tlmgr update --self --all
-#tlmgr init-usertree 
 tlmgr init-usertree 
 #
 tlmgr repository add https://cran.asia/KTUG/texlive/tlnet/ ktug
 #tlmgr pinning add ktug "*"
-#
 tlmgr install collection-langkorean
 
 updmap -sys
